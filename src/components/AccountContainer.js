c0b1a0159f7497d72886e527aa77c60118c56253
Natalies-Angels <natalie.shikuku@student.moringaschool.com>
2023-09-15 12:44:22 +0300

    Completed

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TransactionsList from "./TransactionsList";
-import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+import Search from "./Search"; 
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8002/transactions")
+      .then((response) => response.json())
+      .then((transactions) => setTransactions(transactions))
+      .catch((error) => console.error("Error fetching transactions: ", error));
+  }, []);
+
+  function addNewTransaction(newTransaction) 
+  {
+    setTransactions([...transactions, newTransaction]); 
+  }
+
+  
+  function handleSearch(searchInput) {
+    setSearchTerm(searchInput);
+  }
+
+  const filteredTransactions = transactions.filter((transaction) => {
+    const description = transaction.description || ""; //the empty string handles the empty descriptions
+    return description.toLowerCase().includes(searchTerm.toLowerCase()); //toLowerCase() removes case insensitivity
+  });
+
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <h2>Transaction List</h2>
+      <Search onSearch={handleSearch} /> 
+      <AddTransactionForm onAddTransaction={addNewTransaction} />
+      <TransactionsList transactions={filteredTransactions} />
     </div>
   );
 }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddTransactionForm() {
+function AddTransactionForm( {onAddTransaction}) {
+  const [formData, setFormData] = useState({
+      date : "",
+      description: "",
+      category : "",
+      amount : "",
+  }); //formData is set to an object
+  //The formData object collects and organizes the data from the form before sending it to the server
+  
+  function updateTransactions(event)
+  {
+    const{name, value} = event.target; //goes through the .target method and aasigns name and value to the variables name,value
+    setFormData({...formData,[name]:value});//having name in [] lets react know that 'name' is the key and 'value' is the value
+  }
+  function handleSubmit(event)
+  {
+    event.preventDefault();
+    const transactionData = {
+      date:formData.date,
+      description: formData.description,
+      category: formData.category,
+      amount: formData.amount,
+    }; //assigning empty strings to the keys which will later be overwritten by the data from the API
+
+    fetch("http://localhost:8002/transactions",
+    {
+      method: "POST",
+      headers: 
+      {
+        "Content-Type" :"application/json",
+      },
+      body: JSON.stringify(transactionData),
+    })
+    .then((response) => response.json())
+    .then((newTransaction) => onAddTransaction(newTransaction)); 
+    
+  }
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form className="ui form" onSubmit={handleSubmit}>
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input type="date" name="date" value={formData.date} onChange={updateTransactions}/>
+          <input type="text" name="description" placeholder="Description" value={formData.description} onChange={updateTransactions} />
+          <input type="text" name="category" placeholder="Category" value={formData.category} onChange={updateTransactions}/>
+          <input type="number" name="amount" placeholder="Amount" step="0.01" value={formData.amount} onChange={updateTransactions}/>
         </div>
         <button className="ui button" type="submit">
           Add Transaction
